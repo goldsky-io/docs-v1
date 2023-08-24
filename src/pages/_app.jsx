f70f5c69eff684e45a4ed376c6b59615c76e5859
John Clover <radiofreejohn@users.noreply.github.com>
2023-08-24 15:59:18 -0400

    remove heap, add GA

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -10,39 +10,15 @@ if (
   typeof window !== "undefined" &&
   window.location.host === "docs.goldsky.com"
 ) {
-  (window.heap = window.heap || []),
-    (heap.load = function (e, t) {
-      (window.heap.appid = e), (window.heap.config = t = t || {});
-      var r = document.createElement("script");
-      (r.type = "text/javascript"),
-        (r.async = !0),
-        (r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js");
-      var a = document.getElementsByTagName("script")[0];
-      a.parentNode.insertBefore(r, a);
-      for (
-        var n = function (e) {
-            return function () {
-              heap.push([e].concat(Array.prototype.slice.call(arguments, 0)));
-            };
-          },
-          p = [
-            "addEventProperties",
-            "addUserProperties",
-            "clearEventProperties",
-            "identify",
-            "resetIdentity",
-            "removeEventProperty",
-            "setEventProperties",
-            "track",
-            "unsetEventProperty",
-          ],
-          o = 0;
-        o < p.length;
-        o++
-      )
-        heap[p[o]] = n(p[o]);
-    });
-  heap.load("303099433");
+  var r = document.createElement('script');
+  r.src = "https://www.googletagmanager.com/gtag/js?id=G-PGSF5CGSKR";
+  r.async = true;
+  var i = document.getElementsByTagName('script')[0]
+  i.parentNode.insertBefore(r, i)
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-PGSF5CGSKR');
 }
 
 function getNodeText(node) {
